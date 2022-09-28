0e544cb9d8421918946f2b420e15691345c055de
ritesh gurjar <gurjarritesh155@gmail.com>
2022-09-28 19:55:41 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
+import Head from "next/head"
+
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <title>Nft Marketplace</title>
+                <meta name="description" content="NFt marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
 }
 
 export default MyApp
